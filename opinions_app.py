b8e703d3e7f93ecb907305ab1c7c12e8f9c423f0
Dmitrii Maiorov <dimayorov@yandex.ru>
2026-02-05 16:07:58 +0300

    add forms

--- opinions_app.py
+++ opinions_app.py
@@ -1,17 +1,23 @@
 # what_to_watch/opinions_app.py
 
 from datetime import datetime
-
-# Импортировать функцию для выбора случайного значения.
 from random import randrange
 
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
+
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.json.ensure_ascii = False
+app.config['SECRET_KEY'] = 'MY SECRET KEY'
+
 db = SQLAlchemy(app)
+
 
 class Opinion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,19 +26,51 @@ class Opinion(db.Model):
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
+
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'), Length(1, 128)],
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')],
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()],
+    )
+    submit = SubmitField('Добавить')
+
+
 @app.route('/')
 def index_view():
-    # Определить количество мнений в базе данных.
     quantity = Opinion.query.count()
-    # Если мнений нет...
     if not quantity:
-        # ...то вернуть сообщение:
-        return 'В базе данных мнений о фильмах нет.'
-    # Иначе выбрать случайное число в диапазоне от 0 до quantity...
+        return 'В базе данных записей нет.'
     offset_value = randrange(quantity)
-    # ...и определить случайный объект.
     opinion = Opinion.query.offset(offset_value).first()
-    return opinion.text
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_opinion_view():
+    form = OpinionForm()
+    if form.validate_on_submit():
+        opinion = Opinion(
+            title=form.title.data, text=form.text.data, source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+    return render_template('add_opinion.html', form=form)
+
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
+
 
 if __name__ == '__main__':
     app.run()
