f6a3f1f9061c27abc7be3c33ed931b05fb21ba67
Dmitrii Maiorov <dimayorov@yandex.ru>
2026-02-06 12:42:13 +0300

    19th sprint finished

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -6,14 +6,14 @@ from . import app, db
 from .forms import OpinionForm
 from .models import Opinion
 
-@app.route('/')
-def index_view():
-    quantity = Opinion.query.count()
-    if not quantity:
-        abort(500)
-    offset_value = randrange(quantity)
-    opinion = Opinion.query.offset(offset_value).first()
-    return render_template('opinion.html', opinion=opinion)
+# @app.route('/')
+# def index_view():
+#     quantity = Opinion.query.count()
+#     if not quantity:
+#         abort(500)
+#     offset_value = randrange(quantity)
+#     opinion = Opinion.query.offset(offset_value).first()
+#     return render_template('opinion.html', opinion=opinion)
     
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -37,4 +37,21 @@ def add_opinion_view():
 @app.route('/opinions/<int:id>')
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
+
+# what_to_watch/opinions_app/views.py
+
+def random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return opinion
+
+@app.route('/')
+def index_view():
+    opinion = random_opinion()
+    # Если random_opinion() вернула None, значит, в БД нет записей.
+    if opinion is None:
+        abort(500)
     return render_template('opinion.html', opinion=opinion)
